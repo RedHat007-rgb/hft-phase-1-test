53914354bf13cdb70c4f6164b462a35404cd06dd
RedHat007-rgb <cu.16bcs1288@gmail.com>
2025-09-19 13:17:58 +0530

    UPdated wsgateway

--- apps/backend/src/redis/redis.gateway.ts
+++ apps/backend/src/redis/redis.gateway.ts
@@ -1,9 +1,37 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import {
+  OnGatewayConnection,
+  OnGatewayInit,
+  WebSocketGateway,
+} from '@nestjs/websockets';
+import { RedisService } from './redis.service';
+import { InternalServerErrorException } from '@nestjs/common';
 
 @WebSocketGateway()
-export class RedisGateway {
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
+export class RedisGateway implements OnGatewayConnection, OnGatewayInit {
+  constructor(private redisService: RedisService) {}
+  afterInit() {
+    console.log('websocket initialized...');
+  }
+  async handleConnection(client: WebSocket) {
+    if (
+      client.readyState === WebSocket.CLOSED ||
+      client.readyState === WebSocket.CLOSING
+    ) {
+      return;
+    }
+    try {
+      await this.redisService.subscribeMessage(
+        'market.ticker.BTCUSDT.binance',
+        (message) => {
+          if (client.readyState === WebSocket.OPEN) {
+            client.send(message);
+          }
+        },
+      );
+    } catch {
+      throw new InternalServerErrorException(
+        'error occured .... in redis gateway...',
+      );
+    }
   }
 }
