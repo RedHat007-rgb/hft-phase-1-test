ece1ea1227ab7c981dcd84febcd405731392b8d9
RedHat007-rgb <cu.16bcs1288@gmail.com>
2025-09-19 11:46:46 +0530

    Configured redis client

---
+++ packages/redis-client/src/index.ts
@@ -0,0 +1,11 @@
+import { createClient, RedisClientType } from "redis";
+
+export const redisClient: RedisClientType = createClient({
+  url: process.env.REDIS_URL ?? "redis://localhost:6379",
+});
+
+redisClient.on("error", (err) => {
+  console.log("error connecting  to redis ", err);
+});
+
+export default redisClient;
