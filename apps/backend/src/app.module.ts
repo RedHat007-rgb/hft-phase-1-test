4bb4000d19c46b777ad9633363572c4784a7732d
RedHat007-rgb <cu.16bcs1288@gmail.com>
2025-09-19 13:20:42 +0530

    added config

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppDataSource } from '@repo/db';
+import { RedisModule } from './redis/redis.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      ...AppDataSource.options,
+      autoLoadEntities: true,
+      migrationsRun: true,
+      synchronize: false,
+    }),
+    RedisModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/backend/src/redis/redis.service.ts
+++ apps/backend/src/redis/redis.service.ts
@@ -1,4 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import redisClient from '@repo/redis-client';
+import { RedisClientType } from 'redis';
 
 @Injectable()
-export class RedisService {}
+export class RedisService implements OnModuleInit, OnModuleDestroy {
+  private client: RedisClientType;
+  onModuleInit() {
+    this.client = redisClient;
+  }
+
+  async onModuleDestroy() {
+    await this.client.quit();
+  }
+
+  async subscribeMessage(channel: string, callback: (msg: string) => void) {
+    await this.client.subscribe(channel, callback);
+  }
+
+  async redisSubscribe(channel: string, callback: (msg) => void) {
+    await this.client.subscribe(channel, callback);
+  }
+}
